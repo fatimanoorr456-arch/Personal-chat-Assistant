e30664b7f8afe04d55e92a80a9e91964a6449b2a
fatimanoorr456-arch <fatimanoorr456@gmail.com>
2026-06-02 02:23:55 +0500

    Add file

---
+++ main.py
@@ -0,0 +1,56 @@
+#Rule Based AI Python ChatBot 
+
+import datetime
+import time
+
+name= input("Hi, enter your name : ")   #Get user name
+
+
+presentHour= datetime.datetime.now().hour
+
+if 5 <= presentHour <= 11: 
+    print("Good morning, ", name)
+elif 11 <= presentHour <= 17:
+    print("Good afternoon, ", name)
+elif 17 <= presentHour <= 20:
+    print("Good evening, ", name)
+else:
+    print("Good night, ", name)
+
+
+
+print("Hello! Welcome to Your ChatBot")
+print("You can ask me basic question, Type 'bye' to exit from the bot")
+
+# Chatbot Memory Creation  (dictionary of responses )
+
+responses = {
+    "hello": "Hi, welcome. How can I help you?",
+    "how are you": "I am very fine. Thank you",
+    "who are you": "I am smart AI chatbot",
+    "motivate me": "Keep going. Every bug of your project makes you a better developer",
+    "happy": "Great to hear that",
+    "what are functions": "A function is a reusable block of code that performs a specific task."
+} 
+
+# Method/Function to get response of ChatBot 
+
+def getResponseOfBot(userQuestion):
+    userQuestion= userQuestion.lower()
+    for eachKey in responses:
+        if eachKey in userQuestion:
+            return responses[eachKey]
+
+    return "Sorry, I don't know the answer to that question."    
+    
+
+# Take user input 
+
+
+while True:
+    userInput= input("Please ask your question:")
+    reply= getResponseOfBot(userInput)
+    print("Bot Response:", reply)
+
+    if "bye" in userInput.lower():
+        break
